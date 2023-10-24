f775348094cb893c22310dfaa7832edabf5da4f3
SC <sofiane.chahra@stagiairesmns.fr>
2023-10-24 20:41:11 +0200

    commit exercice 24-10-23

---
+++ my-react-app/src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    <div>
+      <header>
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* <a
+          className="App-link"
+          href="http://localhost:8080/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Accéder à la BDD
+        </a> */}
+        <a href="http://localhost:8080/" target="_blank" rel="noreferrer">
+          <button> Accéder à la BDD </button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
